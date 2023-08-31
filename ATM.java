792fe03b3e632c09d94238479b337630bbd27453
Grady <gradyramberg@gmail.com>
2023-08-31 10:00:15 -0700

    Add OpenAccount Method

--- ATM.java
+++ ATM.java
@@ -1,9 +1,17 @@
 import java.util.HashMap;
 
 public class ATM {
-    HashMap<String, String> accountMap;
+    HashMap<String, Double> accountMap;
 
     public ATM() {
-        accountMap = new HashMap<String, String>();
+        accountMap = new HashMap<String, Double>();
+    }
+
+    public void openAccount(String userID, double amount) throws Exception {
+        if (accountMap.containsKey(userID)) {
+            throw new Exception("User already exists");
+        } else {
+            accountMap.put(userID, amount);
+        }
     }
 }
